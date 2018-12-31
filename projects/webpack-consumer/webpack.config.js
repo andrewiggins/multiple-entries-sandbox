5bb4396de2d9ba9b35f5f33308bd58fcb9131cae
Andre Wiggins <andrewiggins@live.com>
2018-12-30 23:54:18 -0800

    Add html page to webpack-consumer

--- projects/webpack-consumer/webpack.config.js
+++ projects/webpack-consumer/webpack.config.js
@@ -1,11 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const outputPath = path.resolve(__dirname, "dist");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
     filename: "webpack-consumer.js",
-    path: path.resolve(__dirname, "dist")
+    path: outputPath
   },
-  devtool: "source-map"
+  plugins: [
+    new CleanWebpackPlugin([outputPath]),
+    new HtmlWebpackPlugin({
+      title: "Webpack Consumer"
+    })
+  ],
+  devtool: "source-map",
+  devServer: {
+    contentBase: outputPath
+  }
 };
